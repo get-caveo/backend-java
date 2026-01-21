12e882b7fea12044c5e0bdd9f4fb11d8db91129d
Wintlink <140983657+Wintlink@users.noreply.github.com>
2026-01-21 14:58:53 +0100

    feat: ajout API publique et système authentification

---
+++ src/main/java/com/caveo/backend/backend/dto/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.caveo.backend.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String prenom;
+    private String nom;
+    private String telephone;
+}
